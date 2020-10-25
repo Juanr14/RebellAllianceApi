9d7c1a15ad130fd53ee4460710e4f3f5e5393305
Juan Davd Rincon <jrincondelga@dxc.com>
2020-10-24 22:48:15 -0500

    Se crea caparazon del service

---
+++ src/main/java/co/com/mercadolibre/rebell/alliance/service/ITopSecretService.java
@@ -0,0 +1,8 @@
+package co.com.mercadolibre.rebell.alliance.service;
+
+import co.com.mercadolibre.rebell.alliance.dto.response.TopSecretResponseDTO;
+
+public interface ITopSecretService {
+
+	public TopSecretResponseDTO identifyMessage();
+}
---
+++ src/main/java/co/com/mercadolibre/rebell/alliance/service/impl/TopSecretServiceImpl.java
@@ -0,0 +1,17 @@
+package co.com.mercadolibre.rebell.alliance.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import co.com.mercadolibre.rebell.alliance.dto.response.TopSecretResponseDTO;
+import co.com.mercadolibre.rebell.alliance.service.ITopSecretService;
+
+@Service
+public class TopSecretServiceImpl implements ITopSecretService{
+
+	@Override
+	public TopSecretResponseDTO identifyMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
